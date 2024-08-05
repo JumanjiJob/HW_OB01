eee33842f302729d1a058aaeff55674dd70d1b45
JumanjiJob <35mirzhan@mail.ru>
2024-08-05 13:25:48 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+class Task():
+    def __init__(self, description, time, status):
+        self.description = description
+        self.time = time
+        self.status = status
+
+    def add (self):
+        
